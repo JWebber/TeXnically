6c45292ec055965999a192d905ae98492a67f03d
JWebber <josephwebber@outlook.com>
2024-12-21 19:21:09 +0000

    Add project files.

---
+++ LatexTools.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TeXnically
+{
+    public class LatexTools
+    {
+        public static string TagSvgWithLatex(string tag, string svg)
+        {
+            // If string is empty, return an empty SVG
+            if (String.IsNullOrWhiteSpace(svg))
+                return "";
+            
+            // Load the SVG content
+            XmlDocument doc = new XmlDocument();
+            try 
+            { 
+                doc.LoadXml(svg);
+            }
+            catch { throw new Exception("The output SVG doesn't appear to be valid XML, and can't be parsed."); }
+
+            if (!doc.HasChildNodes)
+                throw new Exception("The output SVG has no child nodes, and can't be parsed.");
+            else
+            {
+                foreach (XmlNode nd in doc.LastChild.ChildNodes)
+                {
+                    // Tag any g nodes with an id that matches the LaTeX content
+                    if (nd.Name == "g")
+                    {
+                        XmlAttribute newAttr = doc.CreateAttribute("id");
+                        newAttr.Value = tag;
+                        if (nd.Attributes != null)
+                            nd.Attributes.Append(newAttr);
+                    }
+                }
+
+                // Return the modified document
+                return doc.InnerXml.Replace("currentColor", "black"); // MS Office often struggles with this fill/stroke definition
+            }
+        }
+
+        public static string? ReadLatexSvg(string svg)
+        {
+            // Load the XML
+            XmlDocument doc = new XmlDocument();
+            try
+            {
+                doc.LoadXml(svg);
+            }
+            catch
+            {
+                throw new Exception("The input doesn't appear to be valid XML, and can't be parsed.");
+            }
+
+            if (!doc.HasChildNodes)
+                throw new Exception("The input SVG has no child nodes, and can't be parsed.");
+            else
+            {
+                foreach (XmlNode nd in doc.LastChild.ChildNodes)
+                {
+                    if (nd.Name == "g")
+                    {
+                        if (nd.Attributes != null)
+                            return nd.Attributes["id"].Value;
+                    }
+                }
+
+                return null;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Threading;
+using System.Windows.Input;
+using System.Xml;
+using System.Windows.Threading;
+using System.CodeDom;
+using Microsoft.Web.WebView2.Core;
+using System.Reflection;
+using System.Windows.Shapes;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.RegularExpressions;
+using System.Runtime.CompilerServices;
+using System.Diagnostics.Eventing.Reader;
+using System.Windows.Media;
+using Wpf.Ui.Controls;
+
+namespace TeXnically
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        System.Windows.Forms.NotifyIcon trayIcon;
+        private WindowState storedWindowState = WindowState.Normal;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void trayIcon_Click(object? sender, EventArgs e)
+        {
+            Show();
+            WindowState = storedWindowState;
+        }
+
+        const string baselineHtml = "<html><head><script src=\"https://polyfill.io/v3/polyfill.min.js?features=es6\"></script>\r\n<script id=\"MathJax-script\" async src=\"https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js\"></script></head><body><p>***</p></body></html>";
+        bool isLoaded = false;
+        string svgContent = "";
+        Regex rgGetSvgPart = new Regex("<svg.+?</svg>");
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Initialise the UI
+            Wpf.Ui.Appearance.Theme.Apply(Wpf.Ui.Appearance.ThemeType.Light, Wpf.Ui.Appearance.BackgroundType.Acrylic, false);
+
+            // Load the minimise-to-tray UI
+            trayIcon = new System.Windows.Forms.NotifyIcon();
+            trayIcon.Text = "TeXnically";
+            trayIcon.Icon = new System.Drawing.Icon("texnically.ico");
+            trayIcon.Click += new EventHandler(trayIcon_Click);
+        }
+
+        private void postedSvgFromWeb(object? sender, CoreWebView2WebMessageReceivedEventArgs e)
+        {
+            // Pull the raw SVG from the MathJax
+            svgContent = rgGetSvgPart.Match(e.TryGetWebMessageAsString()).Value;
+        }
+
+        private void btnCompile_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(LatexTools.TagSvgWithLatex(tbInput.Text, svgContent));
+            System.IO.MemoryStream stream = new System.IO.MemoryStream(bytes, true);
+            Clipboard.SetData("image/svg+xml", stream);
+            stream.Close();
+            stream.Dispose();
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.IO.MemoryStream dataStream = (MemoryStream)Clipboard.GetData("image/svg+xml");
+                tbInput.Text = LatexTools.ReadLatexSvg(Encoding.UTF8.GetString(dataStream.ToArray()));
+
+                // Refresh the status bar
+                tbkStatus.Text = "Loaded from clipboard; " + Math.Floor(sldZoom.Value).ToString() + "% zoom";
+                tbkStatus.FontWeight = FontWeights.Regular;
+                tbkStatus.Foreground = new SolidColorBrush(Colors.Black);
+            }
+            catch
+            {
+                // Fail somewhat quietly
+                throwError("Unable to read valid SVG from clipboard");
+            }
+        }
+
+        private async void tbInput_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (wbPreview != null && wbPreview.CoreWebView2 != null)
+            {
+                string sanitisedInput = System.Web.HttpUtility.JavaScriptStringEncode(tbInput.Text);
+                var funcall = "convert(\"" + sanitisedInput + "\")";
+                await wbPreview.CoreWebView2.ExecuteScriptAsync(funcall);
+            }
+
+            // Refresh the status bar
+            tbkStatus.Text = "Ready; " + Math.Floor(sldZoom.Value).ToString() + "% zoom";
+            tbkStatus.FontWeight = FontWeights.Regular;
+            tbkStatus.Foreground = new SolidColorBrush(Colors.Black);
+        }
+
+        private void throwError(string errorText)
+        {
+            tbkStatus.Text = errorText;
+            tbkStatus.FontWeight = FontWeights.Bold;
+            tbkStatus.Foreground = new SolidColorBrush(Colors.Red);
+        }
+
+        private void loaded(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            if(!isLoaded && wbPreview != null && wbPreview.CoreWebView2 != null)
+            {
+                string path = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "boilerplate.html");
+                wbPreview.CoreWebView2.Navigate(@"file:///" + path);
+                wbPreview.CoreWebView2.WebMessageReceived += postedSvgFromWeb;
+
+                isLoaded = true;
+            }
+
+        }
+
+        private void Grid_DragEnter(object sender, DragEventArgs e)
+        {
+            // Prepare the grid to show it's ready to receive a drop
+            rectReady.Visibility = Visibility.Visible;
+        }
+
+        private void Grid_Drop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                System.IO.MemoryStream dataStream = (MemoryStream)e.Data.GetData("image/svg+xml");
+                tbInput.Text = LatexTools.ReadLatexSvg(Encoding.UTF8.GetString(dataStream.ToArray()));
+            }
+            catch
+            {
+                // Fail somewhat quietly
+                throwError("Unable to read valid SVG from clipboard");
+            }
+            finally
+            {
+                rectReady.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void Grid_DragLeave(object sender, DragEventArgs e)
+        {
+            rectReady.Visibility = Visibility.Hidden;
+        }
+
+        private void Grid_DragOver(object sender, DragEventArgs e)
+        {
+            // Prepare the grid to show it's ready to receive a drop
+            rectReady.Visibility = Visibility.Visible;
+        }
+
+        private void sldZoom_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (wbPreview != null && wbPreview.CoreWebView2 != null)
+            {
+                wbPreview.ZoomFactor = sldZoom.Value / 100;
+
+                // Refresh the status bar
+                tbkStatus.Text = "Ready; " + Math.Floor(sldZoom.Value).ToString() + "% zoom";
+                tbkStatus.FontWeight = FontWeights.Regular;
+                tbkStatus.Foreground = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            sldZoom.Value = 100;
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+                Hide();
+            else
+                storedWindowState = WindowState;
+        }
+
+        void CheckTrayIcon()
+        {
+            ShowTrayIcon(!IsVisible);
+        }
+
+        void ShowTrayIcon(bool show)
+        {
+            if (trayIcon != null)
+               trayIcon.Visible = show;
+        }
+
+        private void Window_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            CheckTrayIcon();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            trayIcon.Dispose();
+            trayIcon = null;
+        }
+    }
+}
